a26af8f727bf82fa17df3db79f09d13a4bb2c837
cyganouko <kegoyacygan@gmail.com>
2026-05-17 02:41:06 -0600

    fixed static files

--- storiesofhope2/settings.py
+++ storiesofhope2/settings.py
@@ -132,4 +132,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
